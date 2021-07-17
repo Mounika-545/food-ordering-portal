0d2e2aaa19ec4b506b6891c04b92722ab68c63cb
Mounika Jonnala <mounika.sreerangapu@gmail.com>
2021-07-18 01:00:08 +0530

    second commit

--- client/src/components/Cart/Cart.js
+++ client/src/components/Cart/Cart.js
@@ -3,6 +3,8 @@ import { Component } from 'react';
 import { Link } from 'react-router-dom';
 import CartItemsService from '../../actions/cartItems';
 
+import '../PizzaItems/pizzaItems.css';
+
 
 class Cart extends Component {
     constructor(props) {
@@ -29,38 +31,30 @@ class Cart extends Component {
     // }
     render() {
         const allCartItems = this.state.cartItems.map((cart) => (
-            <div className="container">
-
+            <div className="container1">
                 <header>
                     <h5 className="h3">{cart.name}</h5>
                 </header>
-
                 <main className="main">
-
                     <div className="desc">
-
                         <p className="price">Price: {cart.price}</p>
                         <p>Description: {cart.description}</p>
                     </div>
-
                     <div className="middle"></div>
-
                     <div className="imgContainer">
                         <img className="img" src={cart.image} alt={cart.name} />
                     </div>
                 </main>
-
                 <footer>
                     <button className="btn" type="button" onClick={() => this.removeCartItem(cart._id)}>Remove</button>
                 </footer>
-
                 <hr></hr>
             </div>
         ))
         return (
             <div>
                 {allCartItems}
-                <Link className="link" to='/'><button type="button" onClick={() => { }}>Place Order</button></Link>
+                <Link className="button1" to='/'><button type="button" className="btn" onClick={() => { }}>Place Order</button></Link>
             </div>
         )
     }
--- client/src/components/PizzaItems/PizzaItems.js
+++ client/src/components/PizzaItems/PizzaItems.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import CartItemsService from '../../actions/cartItems';
 
+import './pizzaItems.css';
+
 const PizzaItems = (props) => {
     const pizzaItems = props.location.data;
     const createCartItem = (cartItem) => {
@@ -11,21 +13,24 @@ const PizzaItems = (props) => {
     }
     return (
         pizzaItems.map((data) => (
-            <div className="container mt-5">
-                <div className="row">
-                    <header>
-                        <h5>{data.name}</h5>
-                    </header>
-                    <main>
-                        <p>Price: {data.price}</p>
+            <div className="container1">
+                <header>
+                    <h5 className="h3">{data.name}</h5>
+                </header>
+                <main className="main">
+                    <div className="desc">
+                        <p className="price">Price: {data.price}</p>
                         <p>Description: {data.description}</p>
-                        <img src={data.image} alt={data.name} align="right" />
-                    </main>
-                    <footer>
-                        <button type="button" onClick={() => createCartItem(data)}>Order Now</button>
-                    </footer>
-                    <hr></hr>
-                </div>
+                    </div>
+                    <div className="middle"></div>
+                    <div className="imgContainer">
+                        <img className="img" src={data.image} alt={data.name} />
+                    </div>
+                </main>
+                <footer>
+                    <button className="btn" type="button" onClick={() => createCartItem(data)}>Order Now</button>
+                </footer>
+                <hr></hr>
             </div>
         ))
     )
